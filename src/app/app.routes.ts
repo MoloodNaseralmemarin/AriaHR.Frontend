49a7b9c1f6be48f193b7735c44738539c80027e0
Melodi <moloodNaseralmemarin@gmail.com>
2026-08-23 13:55:06 -0700

    import SplashComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './features/auth/pages/login/login-page.component';
 import { VerifyOtpPageComponent } from './features/auth/pages/verify-otp/verify-otp-page.component';
+import { SplashComponent } from './features/auth/pages/splash/splash.component';
 
 export const routes: Routes = [
   {
