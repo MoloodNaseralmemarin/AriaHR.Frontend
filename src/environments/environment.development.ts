63d1237488e88084a3dd08629c9763bfce3587b2
Melodi <moloodNaseralmemarin@gmail.com>
2026-09-23 17:37:53 -0700

    Api Key neshan

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7151',
-  neshanApiKey: '',
+  neshanApiKey: 'web.ab84520bfd15454fa8505e93acae3e86',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: '',
-  neshanApiKey: '',
+  neshanApiKey: 'web.ab84520bfd15454fa8505e93acae3e86',
 };
